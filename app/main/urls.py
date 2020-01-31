8e4b49c352834963f6e15d0998d6d788643d0329
parkhongbeen <pack122@naver.com>
2020-01-31 10:28:39 +0900

    views.py구현

---
+++ app/main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'main'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('add_todo/', views.add_todo, name='add-todo'),
+    path('delete_todo/<int:pk>/', views.delete_todo, name='delete-todo')
+]
--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from .models import Todo
 
-# Create your views here.
+
+def index(request):
+    todo_items = Todo.objects.all().order_by('-added_date')
+    context = {
+        'todo_items': todo_items
+    }
+    return render(request, 'main/index.html', context)
+
+
+def add_todo(request):
+    currunt_date = timezone.now()
+    content = request.POST['content']
+    Todo.objects.create(added_date=currunt_date, text=content)
+    return redirect('main:index')
+
+
+def delete_todo(reqeust, pk):
+    Todo.objects.get(pk=pk).delete()
+    return redirect('main:index')
